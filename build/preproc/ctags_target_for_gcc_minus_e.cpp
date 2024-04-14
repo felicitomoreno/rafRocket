9873372e54e209e2641e123104ca5c37bd538777
David Canchón <dcanchon@gdeinternacional.com>
2024-04-14 08:33:13 -0500

    Configuración del entorno

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,19 +1,61 @@
 # 1 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
-int seconds;
+// #include <MPU6050_tockn.h>
+# 3 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino" 2
+# 4 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino" 2
 
-void setup() {
+MPU6050 sensor;
 
-  Serial.begin(115200);
-  Serial.println("Hola Mundo.");
-  Serial.println();
-  Serial.println();
-  Serial.println("Seconds:");
-  Serial.println();
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+int seconds, milis = 50;
+
+void setup()
+{
+
+    Wire.begin();
+    sensor.initialize();
+
+    Serial.begin(115200);
+    Serial.println("Hola Mundo.");
+    Serial.println();
+    Serial.println();
+    Serial.println("Seconds:");
+    Serial.println();
+
+    if (sensor.testConnection())
+        Serial.println("Sensor iniciado correctamente");
+    else
+        Serial.println("Errror al inciar el sensor");
 }
 
-void loop() {
+void loop()
+{
+    sensor.getAcceleration(&ax , &ay, &az);
+    float acc_and_x = atan(ax/sqrt(pow(ay,2) + pow(az,2))) * (180.0 / 3.1416);
+    float acc_and_y = atan(ay/sqrt(pow(ax,2) + pow(az,2))) * (180.0 / 3.1416);
 
-  seconds += 1;
-  Serial.println(seconds);
-  delay(1000);
+    seconds += 1;
+    Serial.print(seconds);
+    Serial.print("      ");
+    Serial.print("Valor ax: ");
+    Serial.print(ax);
+    Serial.print(", Valor ay: ");
+    Serial.print(ay);
+    Serial.print(", Valor az: ");
+    Serial.print(az);
+    Serial.print(", Inclinación en x: ");
+    Serial.print(acc_and_x);
+    Serial.print(", Inclinación en y: ");
+    Serial.print(acc_and_y);
+
+    sensor.getRotation(&gx , &gy, &gz);
+    Serial.print("            Rotación x: ");
+    Serial.print(gx);
+    Serial.print(", Rotación y: ");
+    Serial.print(gy);
+    Serial.print(", Rotación z: ");
+    Serial.println(gz);
+
+    delay(milis);
 }
--- build/sketch/rocket.ino.cpp
+++ build/sketch/rocket.ino.cpp
@@ -1,25 +1,67 @@
 #include <Arduino.h>
 #line 1 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
-int seconds;
+// #include <MPU6050_tockn.h>
+#include <MPU6050.h>
+#include "Wire.h"
 
-#line 3 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
+MPU6050 sensor;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+int seconds, milis = 50;
+
+#line 12 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
 void setup();
-#line 13 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
+#line 31 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
 void loop();
-#line 3 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
-void setup() {
+#line 12 "C:\\Users\\Admin\\Downloads\\PI 2024-1\\Arduino Code\\rocket\\rocket.ino"
+void setup()
+{
 
-  Serial.begin(115200);
-  Serial.println("Hola Mundo.");
-  Serial.println();
-  Serial.println();
-  Serial.println("Seconds:");
-  Serial.println();
+    Wire.begin();
+    sensor.initialize();
+
+    Serial.begin(115200);
+    Serial.println("Hola Mundo.");
+    Serial.println();
+    Serial.println();
+    Serial.println("Seconds:");
+    Serial.println();
+
+    if (sensor.testConnection())
+        Serial.println("Sensor iniciado correctamente");
+    else
+        Serial.println("Errror al inciar el sensor");
 }
 
-void loop() {
+void loop()
+{
+    sensor.getAcceleration(&ax , &ay, &az);
+    float acc_and_x = atan(ax/sqrt(pow(ay,2) + pow(az,2))) * (180.0 / 3.1416);
+    float acc_and_y = atan(ay/sqrt(pow(ax,2) + pow(az,2))) * (180.0 / 3.1416);
 
-  seconds += 1;
-  Serial.println(seconds);
-  delay(1000);
+    seconds += 1;
+    Serial.print(seconds);
+    Serial.print("      ");
+    Serial.print("Valor ax: ");
+    Serial.print(ax);
+    Serial.print(", Valor ay: ");
+    Serial.print(ay);
+    Serial.print(", Valor az: ");
+    Serial.print(az);
+    Serial.print(", Inclinación en x: ");
+    Serial.print(acc_and_x);
+    Serial.print(", Inclinación en y: ");
+    Serial.print(acc_and_y);
+    
+    sensor.getRotation(&gx , &gy, &gz);
+    Serial.print("            Rotación x: ");
+    Serial.print(gx);
+    Serial.print(", Rotación y: ");
+    Serial.print(gy);
+    Serial.print(", Rotación z: ");
+    Serial.println(gz);
+
+    delay(milis);
 }
